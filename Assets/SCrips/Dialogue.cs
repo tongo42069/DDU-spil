f745e6bda081de6de93820f3a9d3d02d799174b8
Tong <tong.pnk@gmail.com>
2020-11-26 11:09:15 +0100

    Spil Upload

---
+++ Assets/SCrips/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///klassen her indholder alt info som vi har brug for en dialogue 
+
+[System.Serializable]
+public class Dialogue 
+{
+    public string name;
+
+    [TextArea(3,10)]
+    public string[] sentences; 
+ }
---
+++ Assets/SCrips/DialogueManger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManger : MonoBehaviour
+{
+
+
+//mening at jeg skulle have lavet en array, men de forslåer at jeg lave queue. Det her variable holder styr sætninger i spillet. data typen Queue vil load ind den næste dialog
+    private Queue<string> sentences; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    sentences = new Queue<string>();        
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log ("Starting conversation with " + dialogue.name);
+    }
+
+    
+}
